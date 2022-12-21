6f17e1ace8155dd466af55bbc1398cf27d313344
Heckyy <feriwnarta26@gmail.com>
2022-12-21 13:24:47 +0700

    new feature starting balance

--- src/starting_balance/proses/proses.php
+++ src/starting_balance/proses/proses.php
@@ -22,9 +22,10 @@ if (!empty($_POST['proses']) && !empty($_SESSION['id_employee'])) {
 		$nominal = $_POST['nominal'];
 		$note = $_POST['note'];
 		// echo $nominal;
+		$nominal_fix = str_replace(".", "", $nominal);
 
 		if ($nominal != null && $bank != null) {
-			$db->insert("tb_priod", "id_bank_cash='" . $bank . "',saldo_awal='" . $nominal . "',priod='" . $periode_fix . "',note='" . $note . "'");
+			$db->insert("tb_priod", "id_bank_cash='" . $bank . "',saldo_awal='" . $nominal_fix . "',priod='" . $periode_fix . "',note='" . $note . "'");
 		} else {
 			$error = 1;
 			echo $error;
